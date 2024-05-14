955dc6cb417a986c0898a089bd6f21cc6e7eb799
elkdev72 <elijahkanyia.developer@gmail.com>
2024-05-14 23:30:43 +0300

    login  view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            
