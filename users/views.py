14d262cc7e086733f9dd8961d1f5f7f8b0cb8188
elkdev72 <elijahkanyia.developer@gmail.com>
2024-05-14 23:32:27 +0300

    login  view

--- users/views.py
+++ users/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-
+f
 # Create your views here.
 def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            
+            return redirect('login')
+        else:
+            return render(request, 'accounts/register.html', {'form': form})
+        
+        else:
+    form = UserCreationForm()
+    return render(request, 'accounts/register.html', {'form': form})
