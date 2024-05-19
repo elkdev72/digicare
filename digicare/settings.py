b9ef34102ed5d34fb086b57fa34aada3031b04bb
elkdev72 <elijahkanyia.developer@gmail.com>
2024-05-19 21:56:17 +0300

    urls allauth

--- digicare/settings.py
+++ digicare/settings.py
@@ -49,6 +49,8 @@ SOCIALACCOUNT_PROVIDERS = {
                 }
                 }
 }
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
