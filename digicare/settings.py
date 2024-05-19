ecce02e185cca146a4ebd36b5b2c361425ac7045
elkdev72 <elijahkanyia.developer@gmail.com>
2024-05-19 21:47:25 +0300

    auth backends

--- digicare/settings.py
+++ digicare/settings.py
@@ -49,7 +49,10 @@ SOCIALACCOUNT_PROVIDERS = {
                 }
                 }
 }
-AUTHENTICATION
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
 
 
 MIDDLEWARE = [
